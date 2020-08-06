eaa99b966d9284a4f90a832e1bcf8ca6f53b0301
Sean Kramer <seandominickramer@gmail.com>
2020-08-05 20:34:00 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const todos = [
+  {
+    task: '',
+    id: null,
+    completed: false
+  }
+]
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+
+    }
+  }
+  // you will need a place to store your state in this component.
+  // design `App` to be the parent component of your application.
+  // this component is going to take care of state, and any change handlers you need to work with your state
+  render() {
+    return (
+      <div>
+        <h2>Welcome to your Todo App!</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
